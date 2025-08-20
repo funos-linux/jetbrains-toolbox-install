25d95dd7dcee528410b2cb12ba4ee76da6a54bef
FunOS Linux <admin@funos.org>
2025-08-20 12:48:24 +0700

    Update jetbrains-toolbox.sh

--- jetbrains-toolbox.sh
+++ jetbrains-toolbox.sh
@@ -27,10 +27,17 @@ mkdir -p $SYMLINK_DIR
 rm "$SYMLINK_DIR/jetbrains-toolbox" 2>/dev/null || true
 ln -s "$INSTALL_DIR/bin/jetbrains-toolbox" "$SYMLINK_DIR/jetbrains-toolbox"
 
+# Ensure $HOME/.local/bin is in PATH permanently
+if ! grep -q 'export PATH="$HOME/.local/bin:$PATH"' "$HOME/.bashrc"; then
+    echo -e "\e[94mAdding $HOME/.local/bin to PATH in ~/.bashrc...\e[39m"
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
+ 	source ~/.bashrc
+fi
+
 if [ -z "$CI" ]; then
 	echo -e "\e[94mRunning for the first time to set-up...\e[39m"
 	( "$INSTALL_DIR/bin/jetbrains-toolbox" & )
-	echo -e "\n\e[32mDone! JetBrains Toolbox should now be running, in your application list, and you can run it in terminal as jetbrains-toolbox (ensure that $SYMLINK_DIR is on your PATH)\e[39m\n"
+	echo -e "\n\e[32mDone! JetBrains Toolbox should now be running, in your application list, and you can run it in terminal as jetbrains-toolbox\e[39m\n"
 else
 	echo -e "\n\e[32mDone! Running in a CI -- skipped launching the AppImage.\e[39m\n"
 fi
